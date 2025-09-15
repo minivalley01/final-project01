af703723b55088b9836e178260ed57774898d099
minivalley01 <nppot.winas@gmail.com>
2025-09-15 15:23:24 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,36 @@
-# final-project01
-final project for Prongfund
-# hihi
+#include <stdio.h>
+
+
+
+
+int main() {
+    int choice;
+
+    while(1) {
+        printf("\n==== ระบบจัดการข้อมูลการซ่อมแซมรถยนต์ ====\n");
+        printf("1) เพิ่มข้อมูลการซ่อมแซมใหม่\n");
+        printf("2) ค้นหาข้อมูลการซ่อมเเซม\n");
+        printf("3) อัพเดทข้อมูลการซ่อมแซม\n");
+        printf("4) ลบข้อมูลการซ่อมแซม\n");
+        printf("5) แสดงข้อมูลการซ่อมแซมทั้งหมด\n");
+        printf("0) ออกจากโปรแกรม\n");
+        printf("กรุณาเลือกเมนู: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            //case 1: addRepair(); break;
+            //case 2: displayAll(); break;
+            //case 3: searchRepair(); break;
+            //case 4: updateRepair(); break;
+            //case 5: deleteRepair(); break;
+            //case 6: reportSummary(); break;
+            //case 0:
+               // printf("ออกจากโปรแกรมแล้ว ขอบคุณที่ใช้งาน!\n");
+                //exit(0);
+            //default:
+                //printf("เลือกเมนูไม่ถูกต้อง กรุณาลองใหม่\n");
+        //}
+    }
+
+    return 0;
+}
