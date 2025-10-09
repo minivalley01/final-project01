53948bcc19b01e445da336f5530d5e6d618e5adb
minivalley01 <nppot.winas@gmail.com>
2025-10-09 11:00:20 +0700

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-int checkID(char *ID);
+
+#define MAX_RECORDS 100
+
+struct Record {
+    char id[20];
+    char model[50];
+    char problem[200];
+    int cost;
+    int status; 
+
+};
+int loadData(struct Record records[], const char *filename);
+int checkID(const char* filename, const char* id);
 void addRepair();
+
 #endif
