0058e78206651df636af3702d290ddcff67c38a8
minivalley01 <nppot.winas@gmail.com>
2025-10-09 12:48:03 +0700

    Update main.h

--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #define MAX_RECORDS 100
+#define MAX_LINE 256
+
 
 struct Record {
     char id[20];
@@ -11,8 +13,23 @@ struct Record {
     int status; // 1=active, 0=deleted
 };
 
+int confirmAction(const char *message);
+int hasLetterCount(const char *str, int minLetters);
+int hasLetter(const char *s);
+int getIntegerInput(const char *message);
+void toUpperStr(char *str);
+void strToLower(char *str);
+void trim(char *str);
 int loadData(struct Record records[], const char *filename);
+void saveData(struct Record records[], int count, const char *filename);
+void printTable(struct Record records[], int count, const char *filename);
+void printDeletedRecords(struct Record records[], int count);
 int checkID(const char *filename, const char *ID);
 void addRepair();
+void searchRepair();
+void updateRepair(const char *filename);
+void deleteRepair(const char *filename);
+void restoreRepair(const char *filename);
+void deleteOrRestoreMenu();
 
 #endif
